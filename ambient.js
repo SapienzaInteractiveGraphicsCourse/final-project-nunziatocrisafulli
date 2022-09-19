0453988d80ac4a29acd54a74671198e9f5ad0935
Nunziato Crisafulli <crisafulli.1696498@studentiuniroma1.it>
2022-09-19 13:17:00 +0200

    added lawns and street without textures

--- ambient.js
+++ ambient.js
@@ -1,19 +1,43 @@
-class streetLamp {
-    TODO
+class startLawn {
+    constructor() {
+        this.lawnGeometry = new THREE.BoxBufferGeometry(80, 12, 1 );
+        this.lawnMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.lawn = new THREE.Mesh(this.lawnGeometry, this.lawnMaterial);
+        this.lawn.position.set(0,-28,0)
+        scene.add(this.lawn);
+        // var texture = new THREE.TextureLoader().load('texture/startLawn.jpg')
+        // this.material = new THREE.MeshStandardMaterial({ map: texture });
+    }    
 }
 
 class bush {
     TODO    
 }
 
-class finishLawn {
-    TODO    
-}
-
-class startLawn {
-    TODO    
+class streetLamp {
+    TODO
 }
 
 class Street {
-    TODO    
+    constructor() {
+        this.streetGeometry = new THREE.BoxBufferGeometry(80, 45, 1 );
+        this.streetMaterial = new THREE.MeshBasicMaterial( { color: 0xbbbbbb } );
+        this.street = new THREE.Mesh(this.streetGeometry, this.streetMaterial);
+        this.street.position.set(0,0,0)
+        scene.add(this.street);
+        // var texture = new THREE.TextureLoader().load('texture/startLawn.jpg')
+        // this.material = new THREE.MeshStandardMaterial({ map: texture });
+    }    
+}
+
+class finishLawn {
+    constructor() {
+        this.finishLawnGeometry = new THREE.BoxBufferGeometry(80, 12, 1 );
+        this.finishLawnMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.finishLawn = new THREE.Mesh(this.finishLawnGeometry, this.finishLawnMaterial);
+        this.finishLawn.position.set(0,28,0)
+        scene.add(this.finishLawn);
+        // var texture = new THREE.TextureLoader().load('texture/startLawn.jpg')
+        // this.material = new THREE.MeshStandardMaterial({ map: texture });
+    }    
 }
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+var scene, renderer;
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = -1
 
 init();
 
 function createAmbient() {
-    TODO
+    var startL = new startLawn();
+    var street = new Street();
+    var finishL = new finishLawn();
 }
 
 function createObjects() {
@@ -18,25 +25,25 @@ function runGame() {
 }
 
 function init() {
-    width = window.innerWidth,
-    height = window.innerHeight;
+    width = 768,
+    height = 768;
   
     scene = new THREE.Scene();
     
-    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
-    camera.lookAt(scene.position);
-    camera.position.set(0, 0, 0);
-
+    camera = new THREE.PerspectiveCamera(120, width / height, 0.1, 1000);
     renderer = new THREE.WebGLRenderer();
 
-	renderer.setClearColor(0x00000000);
+    renderer.setClearColor(0x00000000);
 	renderer.setSize(width, height);
+    document.body.appendChild(renderer.domElement);
 
-    //createAmbient();
+    createAmbient();
     //createObjects();
     //enableAnimations();
     //runGame();
 
+    camera.position.set(0, 0, 20);
+    camera.lookAt(scene.position);
     render();
 }
 
