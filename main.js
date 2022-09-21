f7355b1040c27e15e7abadabac2448f46116c0e5
Nunziato Crisafulli <crisafulli.1696498@studentiuniroma1.it>
2022-09-21 20:10:16 +0200

    added tractors, spawning of vehicles, frontlights and backlights of vehicles

--- main.js
+++ main.js
@@ -6,21 +6,35 @@ var light, spotLight1, spotLight2;
 var ambient = true, lightSwitch = false;
 
 // objects var
-var startL, startL, street, finishL, bush1, bush2, tree1, tree2, streetL1, streetL2, car, truck, tractor;
-var vehicles = [];
+var startL, startL, street, finishL, bush1, bush2, tree1, tree2, streetL1, streetL2, car, truck, tractor, lightTargetLeft, lightTargetRight, vehicles, spawnPositions, vehiclesColors;
 
-
+function vehiclesAnimation() {
+    for (let i = 0; i < vehicles.length; i++) {
+        var velocity = Math.floor(Math.random() * 2);
+        vehicles[i].startAnimation(velocity);
+    }
+}
 
 function initListeners() {
     document.getElementById("lightSwitch").onclick = function(){lightSwitch = true};
 }
 
 function turnOffVehiclesLights() {
-    TODO
+    for (let i = 0; i < vehicles.length; i++) {
+        vehicles[i].headLight1.intensity = 0.0;
+        vehicles[i].headLight2.intensity = 0.0;
+        vehicles[i].tailLight1.intensity = 0.0;
+        vehicles[i].tailLight1.intensity = 0.0;
+    }
 }
 
 function turnOnVehiclesLights() {
-    TODO
+    for (let i = 0; i < vehicles.length; i++) {
+        vehicles[i].headLight1.intensity = 0.5;
+        vehicles[i].headLight2.intensity = 0.5;
+        vehicles[i].tailLight1.intensity = 0.2;
+        vehicles[i].tailLight1.intensity = 0.2;
+    }
 }
 
 function refreshLight() {
@@ -29,7 +43,7 @@ function refreshLight() {
         scene.remove(light);
         streetL1.headLamp.remove( spotLight1 );
         streetL2.headLamp.remove( spotLight2 );
-        //turnOffVehiclesLights()
+        turnOffVehiclesLights()
         lightSwitch=!lightSwitch;
         ambient=!ambient;
     }
@@ -61,7 +75,7 @@ function refreshLight() {
         spotLight2.shadow.camera.far = 200;
         streetL2.headLamp.add( spotLight2 );
 
-        //turnOnVehiclesLights();
+        turnOnVehiclesLights();
     }
 
     lightSwitch = false;
@@ -77,18 +91,38 @@ function createAmbient() {
     tree2 = new Tree(35);
     streetL1 = new streetLamp(55, -62);
     streetL2 = new streetLamp(-55, 62);
-    refreshLight();
+    lightTargetLeft =  new THREE.Object3D();
+    lightTargetLeft.position.set(-1000,0,0);
+    lightTargetRight = new THREE.Object3D();
+    lightTargetRight.position.set(1000,0,0);
+    scene.add(lightTargetLeft);
+    scene.add(lightTargetRight);
 }
 
 function spawnVehicles() {
-   //car = new Car(0xee4b2b);
-   //scene.add(car.centralBlock)
-
-   //truck = new Truck();
-   //scene.add(truck.frontBlock)
-
-   //tractor = new Tractor();
-   //scene.add(tractor.frontBlock)
+    vehicles = [];
+    spawnPositions = [[-60, -37.5], [-60, -12.5], [60, 12.5], [60,37.5]]
+    vehiclesColors = [0xff5733, 0xffbb33, 0xc1f33, 0x33ff6e, 0x33ffec, 0x335eff, 0xce33ff, 0xff3171];
+    for (let i = 0; i < spawnPositions.length; i++) {
+        var color = vehiclesColors[Math.floor(Math.random() * vehiclesColors.length)]
+        var vehicleIndex = Math.floor(Math.random() * 3);
+        if (vehicleIndex == 0) {
+            if (spawnPositions[i][0] < 0) var vehicle = new Car(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetRight, lightTargetLeft);
+            else var vehicle = new Car(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetLeft, lightTargetRight);
+            scene.add(vehicle.centralBlock);
+            vehicles.push(vehicle)
+        } else if (vehicleIndex == 1) {
+            if (spawnPositions[i][0] < 0) var vehicle = new Truck(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetRight, lightTargetLeft);
+            else var vehicle = new Truck(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetLeft, lightTargetRight);
+            scene.add(vehicle.frontBlock);
+            vehicles.push(vehicle)
+        } else {
+            if (spawnPositions[i][0] < 0) var vehicle = new Tractor(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetRight, lightTargetLeft);
+            else var vehicle = new Tractor(spawnPositions[i][0], spawnPositions[i][1], color, lightTargetLeft, lightTargetRight);
+            scene.add(vehicle.frontBlock);
+            vehicles.push(vehicle)
+        }
+    }
 }
 
 function enableAnimations() {
@@ -115,10 +149,11 @@ function init() {
     initListeners();
     createAmbient();
     spawnVehicles();
+    refreshLight();
     //enableAnimations();
     //runGame();
 
-    camera.position.set(0, -200, 150); // x, y, z move along y to follow animal
+    camera.position.set(0, -200, 150);
     //camera.position.set(0, 0, 120);
     //camera.position.set(0, -40, 0);
     //camera.position.set(-40, 0, 0);
@@ -130,6 +165,7 @@ function render() {
     if (lightSwitch) {
         refreshLight();
     }
+    //vehiclesAnimation();
     requestAnimationFrame( render );
     renderer.render(scene, camera);    
 }
--- vehicles.js
+++ vehicles.js
@@ -1,5 +1,6 @@
 class Car {
-    constructor(col) {
+    constructor(posX, posY, col, headTarget, tailTarget) {
+        this.posX = posX;
         // central block
         this.carTexture = new THREE.TextureLoader().load('texture/car.jpg');
         this.carTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
@@ -23,7 +24,7 @@ class Car {
         this.rightFrontWheelGeometry = new THREE.CircleGeometry(3,12);
         this.rightFrontWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
         this.rightFrontWheel = new THREE.Mesh(this.rightFrontWheelGeometry, this.rightFrontWheelMaterial);
-        this.rightFrontWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.rightFrontWheel.rotation.x = THREE.Math.degToRad( -90 );
         this.rightFrontWheel.position.set(0,5.1,-2.5)
         this.frontBlock.add(this.rightFrontWheel);
 
@@ -60,7 +61,7 @@ class Car {
         this.rightBackWheelGeometry = new THREE.CircleGeometry(3,12);
         this.rightBackWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
         this.rightBackWheel = new THREE.Mesh(this.rightBackWheelGeometry, this.rightBackWheelMaterial);
-        this.rightBackWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.rightBackWheel.rotation.x = THREE.Math.degToRad( -90 );
         this.rightBackWheel.position.set(0,5.1,-2.5)
         this.backBlock.add(this.rightBackWheel);
 
@@ -86,14 +87,77 @@ class Car {
         this.backBlock.add(this.rightBackLight)
         this.rightBackLight.position.set(5.1,2.5,0);
 
-        this.centralBlock.position.z+=3
-        this.centralBlock.position.y+=10
+        this.centralBlock.position.x = posX
+        this.centralBlock.position.y = posY
+        this.centralBlock.position.z+=1.5
         this.centralBlock.scale.set(0.8, 1, 0.8)
-    }    
+        if (posX<0) this.centralBlock.rotation.z = THREE.Math.degToRad( 180 );
+
+        this.headLight1 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight1.penumbra = 0.05;
+        this.headLight1.decay = 1;
+        this.headLight1.distance = 500;
+        this.headLight1.shadow.mapSize.width = 1024;
+        this.headLight1.shadow.mapSize.height = 1024;
+        this.headLight1.shadow.camera.near = 10;
+        this.headLight1.shadow.camera.far = 200;
+        this.headLight1.target = headTarget;
+        this.headLight1.angle = THREE.Math.degToRad( 30 );
+
+        this.headLight2 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight2.penumbra = 0.05;
+        this.headLight2.decay = 1;
+        this.headLight2.distance = 500;
+        this.headLight2.shadow.mapSize.width = 1024;
+        this.headLight2.shadow.mapSize.height = 1024;
+        this.headLight2.shadow.camera.near = 10;
+        this.headLight2.shadow.camera.far = 200;
+        this.headLight2.target = headTarget;
+        this.headLight2.angle = THREE.Math.degToRad( 30 );
+
+        this.leftFrontLight.add(this.headLight1)
+        this.rightFrontLight.add(this.headLight2)
+
+        this.tailLight1 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight1.penumbra = 0.05;
+        this.tailLight1.decay = 1;
+        this.tailLight1.distance = 500;
+        this.tailLight1.shadow.mapSize.width = 1024;
+        this.tailLight1.shadow.mapSize.height = 1024;
+        this.tailLight1.shadow.camera.near = 10;
+        this.tailLight1.shadow.camera.far = 200;
+        this.tailLight1.target = tailTarget;
+        this.tailLight1.angle = THREE.Math.degToRad( 45 );
+
+        this.tailLight2 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight2.penumbra = 0.05;
+        this.tailLight2.decay = 1;
+        this.tailLight2.distance = 500;
+        this.tailLight2.shadow.mapSize.width = 1024;
+        this.tailLight2.shadow.mapSize.height = 1024;
+        this.tailLight2.shadow.camera.near = 10;
+        this.tailLight2.shadow.camera.far = 200;
+        this.tailLight2.target = tailTarget;
+        this.tailLight2.angle = THREE.Math.degToRad( 45 );
+
+        this.leftBackLight.add(this.tailLight1);
+        this.rightBackLight.add(this.tailLight2)
+    }
+    
+    startAnimation(velocity) {
+        if (this.posX < 0) {
+            this.centralBlock.position.x+=velocity
+            if (this.centralBlock.position.x > 60) this.centralBlock.position.x = this.posX
+        } else {
+            this.centralBlock.position.x-=velocity
+            if (this.centralBlock.position.x < -60) this.centralBlock.position.x = this.posX
+        }
+    }
 }
 
 class Truck {
-    constructor(col) {
+    constructor(posX, posY, col, headTarget, tailTarget) {
+        this.posX = posX;
         this.truckTexture = new THREE.TextureLoader().load('texture/car.jpg');
         this.truckTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
@@ -147,7 +211,7 @@ class Truck {
         this.rightFrontWheelGeometry = new THREE.CircleGeometry(3,12);
         this.rightFrontWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
         this.rightFrontWheel = new THREE.Mesh(this.rightFrontWheelGeometry, this.rightFrontWheelMaterial);
-        this.rightFrontWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.rightFrontWheel.rotation.x = THREE.Math.degToRad( -90 );
         this.rightFrontWheel.position.set(0,5.1,-5)
         this.frontBlock.add(this.rightFrontWheel);
 
@@ -162,7 +226,7 @@ class Truck {
         this.rightBackWheelGeometry = new THREE.CircleGeometry(3,12);
         this.rightBackWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
         this.rightBackWheel = new THREE.Mesh(this.rightBackWheelGeometry, this.rightBackWheelMaterial);
-        this.rightBackWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.rightBackWheel.rotation.x = THREE.Math.degToRad( -90 );
         this.rightBackWheel.position.set(6,5.1,-6.5)
         this.backBlock.add(this.rightBackWheel);
 
@@ -173,13 +237,223 @@ class Truck {
         this.leftBackWheel.position.set(6,-5.1,-6.5)
         this.backBlock.add(this.leftBackWheel);
         
-        this.frontBlock.rotation.z = THREE.Math.degToRad( -45 );
-        this.frontBlock.position.z+=7.5
+        this.frontBlock.position.x = posX
+        this.frontBlock.position.y = posY
+        this.frontBlock.rotation.z = THREE.Math.degToRad( 0 );
+        this.frontBlock.position.z+=8.1
+        if (posX<0) this.frontBlock.rotation.z = THREE.Math.degToRad( 180 );
+
+        this.headLight1 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight1.penumbra = 0.05;
+        this.headLight1.decay = 1;
+        this.headLight1.distance = 500;
+        this.headLight1.shadow.mapSize.width = 1024;
+        this.headLight1.shadow.mapSize.height = 1024;
+        this.headLight1.shadow.camera.near = 10;
+        this.headLight1.shadow.camera.far = 200;
+        this.headLight1.target = headTarget;
+        this.headLight1.angle = THREE.Math.degToRad( 30 );
+
+        this.headLight2 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight2.penumbra = 0.05;
+        this.headLight2.decay = 1;
+        this.headLight2.distance = 500;
+        this.headLight2.shadow.mapSize.width = 1024;
+        this.headLight2.shadow.mapSize.height = 1024;
+        this.headLight2.shadow.camera.near = 10;
+        this.headLight2.shadow.camera.far = 200;
+        this.headLight2.target = headTarget;
+        this.headLight2.angle = THREE.Math.degToRad( 30 );
+
+        this.leftFrontLight.add(this.headLight1)
+        this.rightFrontLight.add(this.headLight2)
+
+        this.tailLight1 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight1.penumbra = 0.05;
+        this.tailLight1.decay = 1;
+        this.tailLight1.distance = 500;
+        this.tailLight1.shadow.mapSize.width = 1024;
+        this.tailLight1.shadow.mapSize.height = 1024;
+        this.tailLight1.shadow.camera.near = 10;
+        this.tailLight1.shadow.camera.far = 200;
+        this.tailLight1.target = tailTarget;
+        this.tailLight1.angle = THREE.Math.degToRad( 45 );
+
+        this.tailLight2 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight2.penumbra = 0.05;
+        this.tailLight2.decay = 1;
+        this.tailLight2.distance = 500;
+        this.tailLight2.shadow.mapSize.width = 1024;
+        this.tailLight2.shadow.mapSize.height = 1024;
+        this.tailLight2.shadow.camera.near = 10;
+        this.tailLight2.shadow.camera.far = 200;
+        this.tailLight2.target = tailTarget;
+        this.tailLight2.angle = THREE.Math.degToRad( 45 );
+
+        this.leftBackLight.add(this.tailLight1);
+        this.rightBackLight.add(this.tailLight2)
     }    
+
+    startAnimation(velocity) {
+        if (this.posX < 0) {
+            this.frontBlock.position.x+=velocity
+            if (this.frontBlock.position.x > 60) this.frontBlock.position.x = this.posX
+        } else {
+            this.frontBlock.position.x-=velocity
+            if (this.frontBlock.position.x < -60) this.frontBlock.position.x = this.posX
+        }
+    }
 }
 
 class Tractor {
-    constructor() {
+    constructor(posX, posY, col, headTarget, tailTarget) {
+        this.posX = posX;
+        this.tractorTexture = new THREE.TextureLoader().load('texture/car.jpg');
+        this.tractorTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
+        // front block
+        this.frontBlockGeometry = new THREE.BoxBufferGeometry(5, 5, 5 );
+        this.frontBlockMaterial = new THREE.MeshPhongMaterial({ map: this.tractorTexture, color: col });
+        this.frontBlock = new THREE.Mesh(this.frontBlockGeometry, this.frontBlockMaterial);
+
+        // frontlights
+        this.leftFrontLightGeometry = new THREE.BoxBufferGeometry (1,1,0);
+        this.frontLightTexture = new THREE.TextureLoader().load('texture/frontLight.jpg');
+        this.frontLightTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+        this.leftFrontLightMaterial = new THREE.MeshPhongMaterial({map: this.frontLightTexture})
+        this.leftFrontLight = new THREE.Mesh(this.leftFrontLightGeometry, this.leftFrontLightMaterial)
+        this.frontBlock.add(this.leftFrontLight)
+        this.leftFrontLight.position.set(-2.51,-1.5,-1.2);
+        this.leftFrontLight.scale.set(0.8, 0.8, 0.8);
+
+        this.rightFrontLightGeometry = new THREE.BoxBufferGeometry (1,1,0);
+        this.rightFrontLightMaterial = new THREE.MeshPhongMaterial({map: this.frontLightTexture})
+        this.rightFrontLight = new THREE.Mesh(this.rightFrontLightGeometry, this.rightFrontLightMaterial)
+        this.frontBlock.add(this.rightFrontLight)
+        this.rightFrontLight.position.set(-2.51,1.5,-1.2);
+        this.rightFrontLight.scale.set(0.8, 0.8, 0.8);
+
+        // back block
+        this.backBlockGeometry = new THREE.BoxBufferGeometry(10, 5, 13 );
+        this.backBlockMaterial = new THREE.MeshPhongMaterial({ map: this.tractorTexture, color: col });
+        this.backBlock = new THREE.Mesh(this.backBlockGeometry, this.backBlockMaterial);
+
+        this.frontBlock.add(this.backBlock);
+        this.backBlock.position.set(7.5,0,4);
+
+        // taillights
+        this.leftBackLightGeometry = new THREE.BoxBufferGeometry (1,1,0);
+        this.backLightTexture = new THREE.TextureLoader().load('texture/backLight.jpg');
+        this.backLightTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+        this.leftBackLightMaterial = new THREE.MeshPhongMaterial({map: this.backLightTexture})
+        this.leftBackLight = new THREE.Mesh(this.leftBackLightGeometry, this.leftBackLightMaterial)
+        this.backBlock.add(this.leftBackLight)
+        this.leftBackLight.position.set(5.1,-1.5,-5);
+
+        this.rightBackLightGeometry = new THREE.BoxBufferGeometry (1,1,0);
+        this.rightBackLightMaterial = new THREE.MeshPhongMaterial({map: this.backLightTexture})
+        this.rightBackLight = new THREE.Mesh(this.rightBackLightGeometry, this.rightBackLightMaterial)
+        this.backBlock.add(this.rightBackLight)
+        this.rightBackLight.position.set(5.1,1.5,-5);
+        this.rightFrontLight.scale.set(0.7, 0.7, 0.7);
+
+        // front wheels
+        this.wheelTexture = new THREE.TextureLoader().load('texture/tractorWheel.jpg');
+        this.wheelTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
         
-    }    
+        this.rightFrontWheelGeometry = new THREE.CircleGeometry(2,12);
+        this.rightFrontWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
+        this.rightFrontWheel = new THREE.Mesh(this.rightFrontWheelGeometry, this.rightFrontWheelMaterial);
+        this.rightFrontWheel.rotation.x = THREE.Math.degToRad( -90 );
+        this.rightFrontWheel.position.set(0,2.51,-2.5)
+        this.frontBlock.add(this.rightFrontWheel);
+
+        this.leftFrontWheelGeometry = new THREE.CircleGeometry(2,12);
+        this.leftFrontWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
+        this.leftFrontWheel = new THREE.Mesh(this.leftFrontWheelGeometry, this.leftFrontWheelMaterial);
+        this.leftFrontWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.leftFrontWheel.position.set(0,-2.51,-2.5)
+        this.frontBlock.add(this.leftFrontWheel);
+
+        // back wheels
+        this.rightBackWheelGeometry = new THREE.CircleGeometry(4,12);
+        this.rightBackWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
+        this.rightBackWheel = new THREE.Mesh(this.rightBackWheelGeometry, this.rightBackWheelMaterial);
+        this.rightBackWheel.rotation.x = THREE.Math.degToRad( -90 );
+        this.rightBackWheel.position.set(0,2.52,-4.5)
+        this.backBlock.add(this.rightBackWheel);
+
+        this.leftBackWheelGeometry = new THREE.CircleGeometry(4,12);
+        this.leftBackWheelMaterial = new THREE.MeshPhongMaterial({map: this.wheelTexture})
+        this.leftBackWheel = new THREE.Mesh(this.leftBackWheelGeometry, this.leftBackWheelMaterial);
+        this.leftBackWheel.rotation.x = THREE.Math.degToRad( 90 );
+        this.leftBackWheel.position.set(0,-2.52,-4.5)
+        this.backBlock.add(this.leftBackWheel);
+
+        this.frontBlock.position.x = posX
+        this.frontBlock.position.y = posY
+        this.frontBlock.rotation.z = THREE.Math.degToRad( 0 );
+        this.frontBlock.position.z+=4.6
+        if (posX<0) this.frontBlock.rotation.z = THREE.Math.degToRad( 180 );
+
+        this.headLight1 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight1.penumbra = 0.05;
+        this.headLight1.decay = 1;
+        this.headLight1.distance = 500;
+        this.headLight1.shadow.mapSize.width = 1024;
+        this.headLight1.shadow.mapSize.height = 1024;
+        this.headLight1.shadow.camera.near = 10;
+        this.headLight1.shadow.camera.far = 200;
+        this.headLight1.target = headTarget;
+        this.headLight1.angle = THREE.Math.degToRad( 30 );
+
+        this.headLight2 = new THREE.SpotLight( 0xffffff, 0.0 );
+        this.headLight2.penumbra = 0.05;
+        this.headLight2.decay = 1;
+        this.headLight2.distance = 500;
+        this.headLight2.shadow.mapSize.width = 1024;
+        this.headLight2.shadow.mapSize.height = 1024;
+        this.headLight2.shadow.camera.near = 10;
+        this.headLight2.shadow.camera.far = 200;
+        this.headLight2.target = headTarget;
+        this.headLight2.angle = THREE.Math.degToRad( 30 );
+
+        this.leftFrontLight.add(this.headLight1)
+        this.rightFrontLight.add(this.headLight2)
+
+        this.tailLight1 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight1.penumbra = 0.05;
+        this.tailLight1.decay = 1;
+        this.tailLight1.distance = 500;
+        this.tailLight1.shadow.mapSize.width = 1024;
+        this.tailLight1.shadow.mapSize.height = 1024;
+        this.tailLight1.shadow.camera.near = 10;
+        this.tailLight1.shadow.camera.far = 200;
+        this.tailLight1.target = tailTarget;
+        this.tailLight1.angle = THREE.Math.degToRad( 45 );
+
+        this.tailLight2 = new THREE.SpotLight( 0xff0000, 0.0 );
+        this.tailLight2.penumbra = 0.05;
+        this.tailLight2.decay = 1;
+        this.tailLight2.distance = 500;
+        this.tailLight2.shadow.mapSize.width = 1024;
+        this.tailLight2.shadow.mapSize.height = 1024;
+        this.tailLight2.shadow.camera.near = 10;
+        this.tailLight2.shadow.camera.far = 200;
+        this.tailLight2.target = tailTarget;
+        this.tailLight2.angle = THREE.Math.degToRad( 45 );
+
+        this.leftBackLight.add(this.tailLight1);
+        this.rightBackLight.add(this.tailLight2);
+    }
+    
+    startAnimation(velocity) {
+        if (this.posX < 0) {
+            this.frontBlock.position.x+=velocity
+            if (this.frontBlock.position.x > 60) this.frontBlock.position.x = this.posX
+        } else {
+            this.frontBlock.position.x-=velocity
+            if (this.frontBlock.position.x < -60) this.frontBlock.position.x = this.posX
+        }
+    }
 }
